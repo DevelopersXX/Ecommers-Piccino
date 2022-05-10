90f73e63d55f4e27e08f1ec1b7003e93f1a9aa7c
DesarrolladoresXX <NPicci19@gmail.com>
2022-05-10 17:42:19 -0300

    Gregue el contador con boton

--- src/components/ItemListContainer.js
+++ src/components/ItemListContainer.js
@@ -1,8 +1,19 @@
 
+import  ItemCount from "./ItemCount";
+ 
  export default function ItemListContainer ({greeting}) {
     return (
+      // const [cont, setCont] = useState(0)
+      // const [fecha, setFecha]= useState("-")
+      // function contar(){
+      //   setCont(cont + 1)
+      //   setFecha(Date())
+      // }
+
       <div className="itemListContainer">
           <h1 className="itemListContainer_title">{greeting}</h1>
+
+          < ItemCount initial={1} stock={7} onAdd={()=>console.log("Agregado al pedido")}/>
       </div>
     );
   };
